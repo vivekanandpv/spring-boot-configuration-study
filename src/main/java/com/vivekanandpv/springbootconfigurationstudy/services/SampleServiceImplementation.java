6dc7d6eea641cf0fc9e86c4b4518937f6fa766c6
vivek <vivekanandpv@outlook.com>
2023-03-16 21:26:02 +0530

    done

--- src/main/java/com/vivekanandpv/springbootconfigurationstudy/services/SampleServiceImplementation.java
+++ src/main/java/com/vivekanandpv/springbootconfigurationstudy/services/SampleServiceImplementation.java
@@ -1,11 +1,15 @@
 package com.vivekanandpv.springbootconfigurationstudy.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SampleServiceImplementation implements SampleService {
+    @Value("${app.message}")
+    private String message;
+
     @Override
     public String getMessage() {
-        return "Static message";
+        return message;
     }
 }
