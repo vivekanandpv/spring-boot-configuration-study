02a7ceb908f5d777219a726a17a49e9d93f595b1
vivek <vivekanandpv@outlook.com>
2023-03-16 21:40:41 +0530

    done

--- src/main/java/com/vivekanandpv/springbootconfigurationstudy/services/SampleServiceImplementation.java
+++ src/main/java/com/vivekanandpv/springbootconfigurationstudy/services/SampleServiceImplementation.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Service;
 public class SampleServiceImplementation implements SampleService {
     private final String message;
 
+    //  we can create different environments as staging, uat, test, preprod, etc
+    //  every environment has its own application-<env>.properties
+    //  to set the current environment, pass the environment variable: SPRING_PROFILES_ACTIVE
+    //  In the absense of such an environment variable, application.properties will be considered
+
     public SampleServiceImplementation(@Value("${app.message}") String message) {
         this.message = message;
     }
