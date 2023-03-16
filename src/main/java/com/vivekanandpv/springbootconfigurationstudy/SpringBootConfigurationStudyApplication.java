18c96acdb1267bb28e1342a34ae41e36c9022850
vivek <vivekanandpv@outlook.com>
2023-03-16 21:17:46 +0530

    Initial commit

---
+++ src/main/java/com/vivekanandpv/springbootconfigurationstudy/SpringBootConfigurationStudyApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootconfigurationstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootconfigurationstudy/SpringBootConfigurationStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootconfigurationstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigurationStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
